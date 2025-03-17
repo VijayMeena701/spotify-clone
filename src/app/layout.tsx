9c02e90184c457178ec23939746e1460307a29ba
VijayMeena701 <govardhanmeena701@gmail.com>
2025-03-17 14:26:08 +0530

    Remove session handling from layout and providers for simplified component structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
-import {auth} from "@/auth";
 
 export const metadata: Metadata = {
   title: "Spotify Clone",
@@ -13,13 +12,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth();
   
   return (
     <html lang="en">
       <head></head>
       <body className="max-w-screen-2xl h-dvh overflow-hidden mx-auto bg-black antialiased text-white">
-        <Providers session={session}>
+        <Providers>
           <main className="flex-1 overflow-y-auto pb-24">
             {children}
           </main>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { PlayerProvider } from "@/lib/PlayerContext";
 
-export default function Providers({ children, session }: { children: React.ReactNode, session: Session|null }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<SessionProvider session={session}>
+		<SessionProvider>
 			<PlayerProvider>
 				{children}
 			</PlayerProvider>
